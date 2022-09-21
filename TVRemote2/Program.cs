9dc077f044f0d52f4ca9ccc22675256326ee734b
Jeel <45571080+j33l@users.noreply.github.com>
2022-09-21 16:24:44 -0700

    works everything fine

--- TVRemote2/Program.cs
+++ TVRemote2/Program.cs
@@ -4,10 +4,36 @@ namespace TVRemote2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
 
+        /*
+         * Monitoring TV screen State changes
+         */
+        static void getScreenStatus(Screen screen)
+        {
+            Console.WriteLine("TV Screen State: ");
+            Console.WriteLine($"#########################\n# Model: {screen.getModel()} \t\t#\n# Power: {screen.getPower()} \t\t#\n# Volume: {screen.getCurrentVolume()} \t\t#\n# Channel: {screen.getCurrentChannel()} \t\t#\n# Mute: {screen.getMute()} \t\t#\n# Brightness: {screen.getBrightness()} \t#\n# Contras: {screen.getContras()} \t\t#\n#########################");
+        }
+
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter your TV Model: (75TU7000)");
+            string model = Console.ReadLine();
+
+            TVRemote2.Screen screen = new TVRemote2.Screen(model);
+            TVRemote2.Remote remote = new TVRemote2.Remote(ref screen);
+
+            /*
+             * Interacting with Remote
+             */
+            while (screen.getPower())
+            {
+                remote.showButtons();
+                getScreenStatus(screen);
+            }
+
+        }
+
+    }
+
+
+}
