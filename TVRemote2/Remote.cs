bb3cf1b3979856dba63d840db152ca02d742a21a
Jeel <45571080+j33l@users.noreply.github.com>
2022-09-21 17:02:08 -0700

    completed settings

---
+++ TVRemote2/Remote.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+
+namespace TVRemote2
+{
+    public class Remote
+    {
+        
+        public Remote(ref Screen screen)
+        {
+            this.model = screen.getModel();
+            this.screen = screen;
+        }
+
+        //static void Main(String[] args)
+        //{
+        //    Console.WriteLine("This is TV Remote Controller.");
+        //}
+
+        /*
+        *  remote state
+        */
+        String model;
+
+        // tv screen deligates here
+        Screen screen;
+
+        public void showButtons()
+        {
+            Console.WriteLine("Enter your command: (0:power, 1:volume up, 2:volume down, 3:channel up, 4:channel down, 5:set channel, 6:mute, 7:settings, 8:smart menu)");
+            string button = Console.ReadLine();
+
+            switch (button)
+            {
+                case "0":
+                    this.screen.changePower();
+                    break;
+                case "1":
+                    this.screen.changeVolume();
+                    break;
+                case "2":
+                    this.screen.changeVolume(false);
+                    break;
+                case "3":
+                    this.screen.changeChannel();
+                    break;
+                case "4":
+                    this.screen.changeChannel(false);
+                    break;
+                case "5":
+                    Console.WriteLine("Enter your channel number: (0-100)");
+                    string channel = Console.ReadLine();
+                    this.screen.setChannel(Int32.Parse(channel));
+                    break;
+                case "6":
+                    this.screen.changeMute();
+                    break;
+                case "7":
+                    bool isSetting = true;
+                    while(isSetting)
+                    {
+                        Console.WriteLine("Enter your command: (0:increase brightness, 1:decrease brightness, 2:increase contras, 3:decrease contras, 4:back)");
+                        string settingButton = Console.ReadLine();
+                        switch (settingButton)
+                        {
+                            case "0":
+                                this.screen.changeBrightness();
+                                break;
+                            case "1":
+                                this.screen.changeBrightness(false);
+                                break;
+                            case "2":
+                                this.screen.changeContras();
+                                break;
+                            case "3":
+                                this.screen.changeContras(false);
+                                break;
+                            case "4":
+                                isSetting = false;
+                                break;
+                            default:
+                                Console.WriteLine("Invalid command.");
+                                break;
+                        }
+                    }
+                    break;
+                case "8":
+                    break;
+                default:
+                    Console.WriteLine("Invalid command.");
+                    break;
+            }
+        }
+    }
+}
---
+++ TVRemote2/Screen.cs
@@ -0,0 +1,165 @@
+﻿using System;
+namespace TVRemote2
+{
+    public class Screen
+    {
+        public Screen(String model)
+        {
+            this.model = model;
+        }
+
+
+        /*
+        *  screen state
+        */
+        private String model;
+        private bool power = true;
+        private int currentVolume; // 0 to 100
+        private int currentChannel;
+        private bool mute;
+        private int brightness = 50;
+        private int contras = 50;
+
+
+        /*
+        * setters
+        */
+        public void changePower()
+        {
+            this.power = !this.power;
+        }
+
+        public void changeVolume(bool increase = true)
+        {
+            if (increase)
+            {
+                if (this.currentVolume < 100)
+                {
+                    this.currentVolume++;
+                }
+            }
+            else
+            {
+                if (this.currentVolume > 0)
+                {
+                    this.currentVolume--;
+                }
+            }
+        }
+
+        public void changeChannel(bool increase = true)
+        {
+            if (increase)
+            {
+                if (this.currentChannel < 100)
+                {
+                    this.currentChannel++;
+                }
+            }
+            else
+            {
+                if (this.currentChannel > 0)
+                {
+                    this.currentChannel--;
+                }
+            }
+        }
+
+        public void setChannel(int newChannel)
+        {
+            if (newChannel >= 0 && newChannel <= 100)
+            {
+                this.currentChannel = newChannel;
+            }
+        }
+
+        public void changeMute()
+        {
+            this.mute = !this.mute;
+        }
+
+        public void changeBrightness (bool increase = true)
+        {
+            if (increase)
+            {
+                if (this.brightness < 100)
+                {
+                    this.brightness++;
+                }
+            }
+            else
+            {
+                if (this.brightness > 0)
+                {
+                    this.brightness--;
+                }
+            }
+        }
+
+        public void changeContras(bool increase = true)
+        {
+            if (increase)
+            {
+                if (this.contras < 100)
+                {
+                    this.contras++;
+                }
+            }
+            else
+            {
+                if (this.contras > 0)
+                {
+                    this.contras--;
+                }
+            }
+        }
+
+
+        // smart menu
+
+
+        /*
+        * getters
+        */
+
+        public bool getPower()
+        {
+            return this.power;
+        }
+
+        public bool getMute()
+        {
+            return this.mute;
+        }
+
+        public int getCurrentVolume()
+        {
+            return this.currentVolume;
+        }
+
+        public String getModel()
+        {
+            return this.model;
+        }
+
+        public int getCurrentChannel()
+        {
+            return this.currentChannel;
+        }
+
+        public int getBrightness()
+        {
+            return this.brightness;
+        }
+
+        public int getContras()
+        {
+            return this.contras;
+        }
+
+        // smart menu
+        
+
+    }
+}
+
